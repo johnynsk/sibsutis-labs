a5d72b3d197b461f33f4bc8ec8dc54a9939241e8
Evgeniy Vasilev <e.vasilev@office.ngs.ru>
2016-11-29 21:50:19 +0000

    lab10: little refactoring

---
+++ lab10/app/constants.h
@@ -0,0 +1,22 @@
+#ifndef PROJECT_APP_CONSTANTS
+#define PROJECT_APP_CONSTANTS
+
+namespace Application {
+    static const int
+        POSITION_BEGIN = -2,
+        POSITION_END = -1,
+
+        MODE_SURNAME = 1,
+        MODE_NUMBER = 2,
+        MODE_POINTS = 4,
+
+        OPERATION_HALT = 0,
+        OPERATION_FILENAME = 1,
+        OPERATION_SHOWALL = 2,
+        OPERATION_SEARCH = 3,
+        OPERATION_INSERT = 4,
+        OPERATION_MODIFY = 5,
+        OPERATION_CREATE = 6;
+}
+
+#endif
